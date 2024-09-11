42d631eca09fb7d829a97959240571ecf4404590
narob1234 <104373022+narob1234@users.noreply.github.com>
2024-09-11 09:15:23 +0700

    Update config.py

--- mobile-mining/config.py
+++ mobile-mining/config.py
@@ -4,8 +4,8 @@ import os, json, requests
 def banner():
     os.system("clear")
     print("\033[1;34;40m")
-    os.system("figlet -f ANSI_Shadow VERUS")
-    os.system("figlet -f digital server version")
+    os.system("figlet -f ANSI_Shadow VERUS MINER")
+    os.system("figlet -f digital Server Version")
     #print("\033[00m\n")
     print("\033[96mDevelop by iTzFiNzAZa\033[0m")
     #print("\033[96m\033[0m")
@@ -23,6 +23,6 @@ def bannerup():
 def banneredit():
     os.system("clear")
     print("\033[1;34;34m")
-    os.system("figlet -f big [-EDIT-]")
+    os.system("figlet -f big [-EDIT MINER-]")
     print("\033[00m\n")
     print(f"\n\033[96mDevelop by iTzFiNzAZa\033[0m\n")
