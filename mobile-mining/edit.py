d73b986a96922fb1be2a5bae7239c39b9a0585fc
narob1234 <104373022+narob1234@users.noreply.github.com>
2024-09-18 23:27:05 +0700

    Update edit.py

--- mobile-mining/edit.py
+++ mobile-mining/edit.py
@@ -16,7 +16,7 @@ def OffMiner():
        pool = input(" pool : ")
        print("\033[35m-----------------------------------------\033[0m")
         
-       print("ตัวอย่าง: \033[RQLmyfkCoDhRf19xCb8nXHstyx4qZXBYW4\033[00m")
+       print("ตัวอย่าง: \033[93mRQLmyfkCoDhRf19xCb8nXHstyx4qZXBYW4\033[00m")
        wallet = input("wallet: ")
        print("\033[35m-----------------------------------------\033[0m")
 
